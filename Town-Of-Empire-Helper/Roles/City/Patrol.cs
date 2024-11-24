abe74ec67a00d0decbf3e815aedc130e9d9ff547
Vladimir Jigalow <vladimirjigalow@gmail.com>
2024-11-24 22:57:13 +0200

    created: Roles.Patrol.cs

---
+++ Town-Of-Empire-Helper/Roles/City/Patrol.cs
@@ -0,0 +1,58 @@
+﻿using Town_Of_Empire_Helper.Entities;
+using Town_Of_Empire_Helper.Entities.RoleInfo;
+
+namespace Town_Of_Empire_Helper.Roles
+{
+    public class Patrol : Role
+    {
+        public Patrol()
+        {
+            RoleConfigurationHandler.Configurate("дозорный", this);
+            RegisterAct(Steps.Other, "проверить", Logic, [new()]);
+        }
+
+        public override string Update(List<Target> targets)
+        {
+            base.Update(targets);
+
+            Acts[Steps.Other].IsReady = null;
+            Acts[Steps.Other].Targets[0].Role = null;
+
+            return string.Empty;
+        }
+
+        private string Logic(List<Target> targets)
+        {
+            var tg = targets[0].Role;
+
+            if (tg == null) 
+                return string.Empty;
+
+            var result = "Посетители:\n";
+            if (targets.Count <= 3) 
+                foreach (var guest in tg.Guests)
+                    result += $"-\t{guest.User.Nickname} " +
+                        $"({guest.User.Number})\n";
+            else
+            {
+                var rand = new Random();
+                int guestIndex;
+                for (int i = 0; i < 3; i++)
+                {
+                    var excludeIndexs = new List<int>();
+                    do 
+                    {
+                        guestIndex = rand.Next(targets.Count);
+                    }
+                    while (excludeIndexs.Contains(guestIndex));
+                    excludeIndexs.Add(guestIndex);
+
+                    result += $"-\t{tg.Guests[guestIndex].User.Nickname} " +
+                        $"({tg.Guests[guestIndex].User.Number})\n";
+                }
+            }
+            
+            return result;
+        }
+    }
+}
